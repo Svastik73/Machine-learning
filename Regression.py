c61502f659b10d283d6dde59f168cbd96e2df99d
volvarz73 <75065361+Svastik73@users.noreply.github.com>
2023-03-05 21:29:02 +0530

    Update Regression.py

--- Regression.py
+++ Regression.py
@@ -20,3 +20,122 @@ y_train=[17.592  ,  9.1302 , 13.662,   11.854   , 6.8233 , 11.886   , 4.3483 , 1
 # x_train is city population /10000
 print ('Number of training examples (m):', len(x_train))
 #number of training examples will be same for x_train and y_train
+plt.scatter(x_train, y_train, marker='x', c='r') 
+
+# Set the title
+plt.title("Profits vs. Population per city")
+# Set the y-axis label
+plt.ylabel('Profit in $10,000')
+# Set the x-axis label
+plt.xlabel('Population of City in 10,000s')
+plt.show()
+
+def compute_cost(x, y, w, b): 
+    m = x.shape[0] 
+    total_cost = 0
+    for i in range(m):
+        total_cost += ((w*x[i]+b - y[i])**2)/(2*m)
+    return total_cost
+# Compute cost with some initial values for paramaters w, b
+initial_w = 2
+initial_b = 1
+
+cost = compute_cost(x_train, y_train, initial_w, initial_b)
+print(type(cost))
+print(f'Cost at initial w: {cost:.3f}')
+
+def compute_gradient(x, y, w, b): 
+ 
+    m = x.shape[0]
+ 
+    dj_dw = 0
+    dj_db = 0
+    for i in range(m):
+        dj_db += (w*x[i]+b - y[i])/m
+        dj_dw += ((w*x[i]+b - y[i])*x[i])/m
+    return dj_dw, dj_db   
+initial_w = 0
+initial_b = 0
+
+tmp_dj_dw, tmp_dj_db = compute_gradient(x_train, y_train, initial_w, initial_b)
+print('Gradient at initial w, b (zeros):', tmp_dj_dw, tmp_dj_db)
+def gradient_descent(x, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters): 
+    """
+    Performs batch gradient descent to learn theta. Updates theta by taking 
+    num_iters gradient steps with learning rate alpha
+    
+    Args:
+      x :    (ndarray): Shape (m,)
+      y :    (ndarray): Shape (m,)
+      w_in, b_in : (scalar) Initial values of parameters of the model
+      cost_function: function to compute cost
+      gradient_function: function to compute the gradient
+      alpha : (float) Learning rate
+      num_iters : (int) number of iterations to run gradient descent
+    Returns
+      w : (ndarray): Shape (1,) Updated values of parameters of the model after
+          running gradient descent
+      b : (scalar)                Updated value of parameter of the model after
+          running gradient descent
+    """
+    
+    # number of training examples
+    m = len(x)
+
+    J_history = []
+    w_history = []
+    w = copy.deepcopy(w_in)  #avoid modifying global w within function
+    b = b_in
+    
+    for i in range(num_iters):
+
+        # Calculate the gradient and update the parameters
+        dj_dw, dj_db = gradient_function(x, y, w, b )  
+
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw               
+        b = b - alpha * dj_db               
+
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            cost =  cost_function(x, y, w, b)
+            J_history.append(cost)
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters/10) == 0:
+            w_history.append(w)
+            print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f}   ")
+        
+    return w, b, J_history, w_history
+initial_w = 0.
+initial_b = 0.
+
+# some gradient descent settings
+iterations = 1500
+alpha = 0.01
+
+w,b,_,_ = gradient_descent(x_train ,y_train, initial_w, initial_b, 
+                     compute_cost, compute_gradient, alpha, iterations)
+print("w,b found by gradient descent:", w, b)
+m = x_train.shape[0]
+
+predicted = np.zeros(m)
+
+for i in range(m):
+    predicted[i] = w * x_train[i] + b
+plt.plot(x_train, predicted, c = "b")
+
+# Create a scatter plot of the data. 
+plt.scatter(x_train, y_train, marker='x', c='r') 
+
+# Set the title
+plt.title("Profits vs. Population per city")
+# Set the y-axis label
+plt.ylabel('Profit in $10,000')
+# Set the x-axis label
+plt.xlabel('Population of City in 10,000s')
+predict1 = 3.5 * w + b
+print('For population = 35,000, we predict a profit of $%.2f' % (predict1*10000))
+
+predict2 = 7.0 * w + b
+print('For population = 70,000, we predict a profit of $%.2f' % (predict2*10000))
